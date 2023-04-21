7f4c7bddada8ad171958136bb24da52eddc35762
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-04-21 15:57:53 +0200

    09-react-router-useNavigate done

--- React/09-react-router-useNavigate/src/index.js
+++ React/09-react-router-useNavigate/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DashboardLayout } from './Layouts/dashboardLayout';
+import { Dashboard } from './components/dashboard';
+import { LoginLayout } from './Layouts/loginLayout';
+import { Login } from './components/login';
+import { Navigation } from './components/navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<LoginLayout />}>
+        <Route path="/" element={<Login />} />
+      </Route>
+
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
