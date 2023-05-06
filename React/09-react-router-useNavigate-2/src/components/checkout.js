a9eb0787abd1fc1910c0617a427314a4d301fb87
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-05-06 11:29:37 +0200

    09-react-router-useNavigate (02) edited and completed

---
+++ React/09-react-router-useNavigate-2/src/components/checkout.js
@@ -0,0 +1,9 @@
+
+
+export function Checkout() {
+    return (
+        <h1 className='checkout-page'>
+            Checkout Page
+        </h1>
+    )
+}
--- React/09-react-router-useNavigate-2/src/index.js
+++ React/09-react-router-useNavigate-2/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App'
 import ShoppingCart from './components/shoppingCart';
+import { Checkout } from './components/checkout'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <ShoppingCart />
+    <Routes>
+      <Route path="/" element={<ShoppingCart />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
   </BrowserRouter >
 );
 
