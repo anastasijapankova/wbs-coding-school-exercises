f5eb4a86aaa5f98c77afffcff19f5386b9d5e90e
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-06-07 22:49:31 +0200

    Node folder added

---
+++ Node/demoImportExport.js
@@ -0,0 +1,8 @@
+// Common Modiles JS
+
+module.exports = { name: "Anastasija", age: 27 }
+module.exports.surname = "Pankova"
+module.exports.city = "Berlin"
+module.exports.function = function (a, b) {
+    return a * b
+}
---
+++ Node/server.js
@@ -0,0 +1,4 @@
+const anyName = require("./demoImportExport.js")
+
+console.log(anyName)
+console.log(anyName.function(2, 3)) 
