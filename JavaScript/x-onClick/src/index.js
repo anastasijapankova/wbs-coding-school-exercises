1f9473e43f00c5d863211f8b1e693828960fbda8
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-06-24 00:00:17 +0200

    x-onClick done

---
+++ JavaScript/x-onClick/src/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.querySelector(".block button");
+    button.addEventListener("click", function () {
+        window.location.href = "https://getbootstrap.com";
+    });
+});
