07a3e5b9a2cbe666974dcd14e819ad96b7c04085
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-07-08 18:45:04 +0200

    x-object done

---
+++ React/x-object/src/index.js
@@ -0,0 +1,12 @@
+const students = [
+    {
+        "first name": "Han",
+        "last name": "Solo"
+    },
+    {
+        "first name": "Chew",
+        "last name": "Bacca"
+    }
+];
+
+console.log(students);
