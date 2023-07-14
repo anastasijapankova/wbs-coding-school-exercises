d120e9a51ad77de1bccd4d321c72791d72d97dc1
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-07-14 23:03:30 +0200

    x-react-props done

---
+++ React/x-react-props/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+import { Student } from "./Student";
+
+export default function App() {
+    return (
+        <div className="App">
+            <div className="block">
+                In this exercise you will have to:
+                <ul>
+                    <li>
+                        Create a Component <b>Student</b>
+                    </li>
+                    <li>
+                        Your Component should be called with this form:{" "}
+                        {'<Student name="John Doe">'} and should display{" "}
+                        <b>Hello John Doe!</b>
+                    </li>
+                </ul>
+                <a
+                    className="link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://reactjs.org/docs/components-and-props.html"
+                >
+                    Help
+                </a>
+            </div>
+            <div className="block">
+                <Student name="John Doe" />
+            </div>
+        </div>
+    );
+}
---
+++ React/x-react-props/src/Student.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Student({ name }) {
+    return <div>Hello {name}!</div>;
+}
